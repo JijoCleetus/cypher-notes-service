c244fb8fda50330ab7ea534756f36d7d6d8fd673
DESKTOP-GQMFN7H\STCLABS <shine.aj@santechlabs.com>
2020-03-15 18:35:41 +0530

    added routes and logic

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const logger = require('./middlewares/logger');
+const mongoose = require('mongoose');
+require('dotenv/config')
 
 const app = express();
 const port = process.env.port || 3000;
@@ -11,13 +13,25 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
 app.use(logger.log);
 
+// import routes
+const messageRoute = require('./routes/messages');
+const pingRoute = require('./routes/ping');
+
+app.use('/message', messageRoute);
+app.use('/ping', pingRoute);
+
 // routes
 app.get('/', (req, res) => {
-    res.send("We are on home");
-})
-app.post('/post', (req, res) => {
-    res.send("We are on post");
-})
+    res.send("Try /ping to check");
+});
+
+//db connect
+mongoose.connect(
+    process.env.DB_CONNECTION,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => {
+        console.log("connected to db")
+    })
 
 // app.listen(3000);
 
